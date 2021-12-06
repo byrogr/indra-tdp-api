33fcd489f6e1f56294f949a77698cb9a4e94a548
Katterine Maguiña <kfmaguina@indracompany.com>
2021-12-06 10:25:34 -0500

    autenticacion

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,30 @@
+const  basicAuth  = require("basic-auth")
+
+const authentication = ( req, res, next ) =>{
+
+    const user = basicAuth(req);
+
+    if (!user || !user.name || !user.pass) {
+        return res.status(401).json({
+            code: 401,
+            failed: true,
+            error: {
+                message: 'Ingresar Datos de Autenticacion'
+            }           
+        });
+    }
+    
+    if (user.name !== 'telefonica' || user.pass !== 'telefonica') {
+        return res.status(401).json({
+            code: 401,
+            failed: true,
+            error: {
+                message: 'Datos de Auntenticacion Incorrectos'
+            }           
+        });
+    } 
+
+    next();
+}
+
+module.exports = authentication
